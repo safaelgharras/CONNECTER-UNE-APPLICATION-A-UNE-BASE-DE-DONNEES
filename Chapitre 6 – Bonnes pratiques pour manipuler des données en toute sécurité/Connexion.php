70922228f59393382681cb2cfd8ca0e454966303
safaelgharras <ehsafaa7@gmail.com>
2026-02-26 11:05:13 +0000

    file

---
+++ Chapitre 6 – Bonnes pratiques pour manipuler des données en toute sécurité/Connexion.php
@@ -0,0 +1,17 @@
+<?php 
+$host = "localhost";
+$dbname = "chapitres";
+$user = "root";
+$password = "";
+
+try{
+    $dsn = "mysql:host=$host;dbname=$dbname";
+    $pdo = new PDO($dsn , $user, $password);
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    //echo "Connexion réussie"; 
+    
+}catch(PDOException $e){
+    echo "Erreur: " . $e->getMessage();
+}
+?>
